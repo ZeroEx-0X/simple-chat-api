cc69055cc2bba590e9be488e6b7c32fbfc6b42e9
ZeroEx-0X <zeroexbot01@gmail.com>
2025-05-23 14:20:47 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import fs from 'fs-extra';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const DB_FILE = './data.json';
 
 const loadDB = async () => {
